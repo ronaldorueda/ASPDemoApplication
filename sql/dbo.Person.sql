0e43e4e7f179c1cd13b2e75aff1e8225c773edc2
ronaldorueda <rueda.ronaldo@hotmail.com>
2020-01-21 11:01:37 -0600

    adding sql server

---
+++ sql/dbo.Person.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(10) NOT NULL, 
+    [LastName] NCHAR(10) NOT NULL, 
+    [Birthday] DATE NOT NULL, 
+    [Description] NVARCHAR(100) NULL
+)
