039f552faf750a09b8f438494a47f0c135f2af72
ronaldorueda <rueda.ronaldo@hotmail.com>
2020-01-23 19:45:15 -0600

    Created model and controller to access database. Will add more to add and delete to database.

---
+++ MVCDemo/Models/PersonDBModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class PersonDBModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Birthdate { get; set; }
+        public string Description { get; set; }
+    }
+}
